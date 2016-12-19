2b547e260334a31e8af09e5dbd72235574dbf41d
muhand <muhandjumah@gmail.com>
2016-12-19 17:12:34 -0500

    Created blank project

---
+++ PredictTestScore/PredictTestScore/main.cpp
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////////////////
+//					Predict the next test scroes
+//							Muhand Jumah
+//							 12/19/2016
+//This project uses neural networks to predict the next test scores 
+//based on hours you studied and hours you slept for previous exams.
+///////////////////////////////////////////////////////////////////
+#include<iostream>
+
+/////////////////////////////
+//Name spaces
+/////////////////////////////
+using namespace std;
+
+int main()
+{
+
+Terminate:	
+	return 0;
+}
